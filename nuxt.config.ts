fa2e093cb90f9e862257eab3e945b48397ea21a0
Crysta1221 <takuyakojim@outlook.jp>
2024-01-13 19:20:13 +0900

    Add OGP

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,23 +1,25 @@
-import Sitemap from "vite-plugin-sitemap";
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
     head: {
       charset: 'utf-16',
       viewport: 'width=device-width',
-      title: 'CrystaWorld',
       link: [
         { rel: 'icon', type: 'image/x-icon', href: '/logo.png' }
       ],
       htmlAttrs: {
         lang: 'ja'
       },
+      meta: [
+        { property: 'og:image', content: '/CrystaWorld.png' },
+      ]
     }
   },
   css: ['~/assets/css/style.scss'],
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/google-fonts", "@nuxtjs/color-mode", "@vueuse/nuxt", "@nuxt/content"],
+  modules: [
+    "@nuxtjs/tailwindcss", "@nuxtjs/google-fonts", "@nuxtjs/color-mode", "@vueuse/nuxt", "@nuxt/content","@nuxtjs/seo"
+  ],
   tailwindcss: {
     cssPath: "~/assets/css/style.scss",
     configPath: 'tailwind.config.js',
@@ -38,5 +40,11 @@ export default defineNuxtConfig({
       'Noto+Sans+JP': true,
       'Inter': true
     }
+  },
+  site: {
+    url: "https://crystaworld1221.com",
+    name: "CrystaWorld",
+    description: "Welcome to CrystaWorld!",
+    defaultLocale: "ja",
   }
 })
